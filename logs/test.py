2b30d3913a95b5692cf60ff275b985808905cde0
Kas-sim <amkassim444@gmail.com>
2026-02-20 06:00:45 +0500

    Overwrite the following code EXACTLY as written (keep struct

---
+++ logs/test.py
@@ -0,0 +1,9 @@
+
+from tools.repo_brain import build_repo_brain
+from agent.llm import call_llm
+
+build_repo_brain("/home/UserX/Master/Operon/", call_llm)
+
+# write summary of tools/repo_brain.py as a comment in
+
+# this comment is added by Operon from MQ
