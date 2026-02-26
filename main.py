57f529c3ee3688eaa5bce8ec06f855d076b0ce00
Kas-sim <amkassim444@gmail.com>
2026-02-27 04:39:33 +0500

    [Operon] Step 1: patched main.py

--- main.py
+++ main.py
@@ -2,8 +2,10 @@
 import sys
 from tui.app import OperonUI
 
+print("hello operon")
 if __name__ == "__main__":
     try:
+        print("hello operon")
         OperonUI().run()
     except KeyboardInterrupt:
         print("\nOperon shut down safely.")
