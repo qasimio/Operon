934c358d88f99d4b8a05dffb22e0d0ddfb78bebe
Kas-sim <amkassim444@gmail.com>
2026-02-27 05:33:13 +0500

    Operon is cutting random pieces and also - if print exists it is thinking no change rquired

--- main.py
+++ main.py
@@ -2,10 +2,12 @@
 import sys
 from tui.app import OperonUI
 
-def count_to_100():
-    for i in range(101):
-        print(i)
+if __name__ == "__main__":
+    try:
+        OperonUI().run()
+    except KeyboardInterrupt:
+        print("\nOperon shut down safely.")
+        sys.exit(0)
 
 def print_universal_operon():
     print("Universal Operon")
-    # this is added by nemotron - Operon professional.
